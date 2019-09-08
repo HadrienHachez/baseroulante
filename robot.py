59fadae9da3ec6aa3544c93adba9b94814247b71
Hadrien Hachez <hadrienhachez@hotmail.com>
2019-09-08 19:44:53 +0200

    get info from motors

--- robot.py
+++ robot.py
@@ -9,9 +9,22 @@ gpio.setup(13, gpio.OUT)
 gpio.setup(15, gpio.OUT)
 
 gpio.output(7, True)
+gpio.output(11, False)
+gpio.output(13, True)
+gpio.output(15, False)
+time.sleep(0.5)
+
+gpio.output(7, False)
 gpio.output(11, True)
 gpio.output(13, True)
 gpio.output(15, False)
 time.sleep(0.5)
+
+gpio.output(7, False)
+gpio.output(11, True)
+gpio.output(13, False)
+gpio.output(15, True)
+time.sleep(0.5)
+
 
 gpio.cleanup()
